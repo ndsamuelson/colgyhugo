5607b38742277203cc5bdfaa52f8b308bbd50dca
Nic Samuelson <ndsamuelson@gmail.com>
2020-03-16 06:25:35 -0500

    yolo

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo -t ananke -D  # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t somrat  # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd docs
